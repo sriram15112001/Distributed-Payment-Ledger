2a3b8226cac1378cdb4d576e95303df67a204520
Deepak Sriram <dsriram@griddynamics.com>
2026-07-08 06:46:42 +0530

    Initial commit

---
+++ src/main/java/com/deepak/distributed_payment_ledger/DistributedPaymentLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.deepak.distributed_payment_ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedPaymentLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedPaymentLedgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepak/distributed_payment_ledger/DistributedPaymentLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.distributed_payment_ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedPaymentLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
